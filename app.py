a128c6e32fc9dd62d304902ec05ef86416634251
Home <yskim@deephelix.net>
2020-10-20 15:27:48 +0900

    Practice graphQL input type

--- app.py
+++ app.py
@@ -1,24 +1,48 @@
+from __future__ import annotations
 import strawberry
 from typing import List
 
 
 @strawberry.type
-class TodoType:
+class date:
+    start: int
+    end: int
+
+
+@strawberry.type
+class todo:
     name: str
     done: bool
+    date: date
+
+
+@strawberry.input
+class sayEmail:
+    email: str
 
 
 def getTodos():
     return [
-        TodoType(name="#1", done=True),
-        TodoType(name="#2", done=True),
-        TodoType(name="#3", done=False),
+        todo(name="#1", done=True, date=date(start=1, end=2)),
+        todo(name="#2", done=True, date=date(start=2, end=3)),
+        todo(name="#3", done=False, date=date(start=3, end=0)),
     ]
 
 
 @strawberry.type
 class Query:
-    TodoTypes: List[TodoType] = strawberry.field(resolver=getTodos)
+    todos: List[todo] = strawberry.field(resolver=getTodos)
 
 
-schema = strawberry.Schema(query=Query)
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def sendEmail(self, info, input: sayEmail) -> str:
+        return f"sending email to {input.email}"
+
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
+
+query = 'mutation { sendEmail(input: { email: "email" }) }'
+result = schema.execute_sync(query)
+print(result)
